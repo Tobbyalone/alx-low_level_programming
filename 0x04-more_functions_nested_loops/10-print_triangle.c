dd59d0af08f5424c8091e2ecb412d8240b612138
Tobi <olorunfemitoi43@gmail.com>
2023-03-23 10:52:35 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
@@ -28,23 +29,4 @@ void print_triangle(int size)
 			_putchar('\n');
 		}
 	}
-}
-
-
-10-main.c
-
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
-    return (0);
 }
